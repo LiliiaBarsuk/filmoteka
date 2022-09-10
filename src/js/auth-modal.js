9e608c35756fb5c5ee8685ababf37cdaf788e349
TaniaSem <semenchuktetiana@gmail.com>
2022-09-10 16:15:27 +0300

    add registration modal

--- src/js/auth-modal.js
+++ src/js/auth-modal.js
@@ -1,10 +1,16 @@
 const authLink = document.querySelector('.authorization');
 const authModal = document.querySelector('.auth-modal');
-const closeBtn = document.querySelector('.modal__button');
+const closeBtn = document.querySelector('#auth-modal-btn');
+const registrLink = document.querySelector('.registration');
+const registrModal = document.querySelector('#registr-modal');
+const registrCloseBtn = document.querySelector('#registr-modal-btn');
 const body = document.querySelector('body');
 
 authLink.addEventListener('click', onClickAuthLink);
 closeBtn.addEventListener('click', onClickCloseBtn);
+registrLink.addEventListener('click', onClickRegistrLink);
+registrCloseBtn.addEventListener('click', onRegistrCloseBtn);
+
 document.addEventListener('keydown', e => {
   if (e.code === 'Escape') {
     onClickCloseBtn();
@@ -17,7 +23,18 @@ function onClickAuthLink(e) {
   body.classList.add('overflow-hidden');
 }
 
-function onClickCloseBtn(e) {
+function onClickCloseBtn() {
   authModal.classList.remove('open');
+  body.classList.remove('overflow-hidden');
+}
+
+function onClickRegistrLink(e) {
+  e.preventDefault();
+  authModal.classList.remove('open');
+  registrModal.classList.add('open');
+}
+
+function onRegistrCloseBtn() {
+  registrModal.classList.remove('open');
   body.classList.remove('overflow-hidden');
 }
